82bfa6f0915736f002a70916f25d45db885eb51b
ArtusAbelard <tribelha@gmail.com>
2024-01-22 16:35:20 +0100

    add comentary

--- public/js/main.js
+++ public/js/main.js
@@ -1,35 +1,30 @@
 let btncalc = document.querySelectorAll(".btncalc")
-let chiffre = document.querySelectorAll(".chiffre")
+let chiffre = document.querySelectorAll(".chiffre")    //! ciblage des elements !
 let inpscreen = document.querySelector("#inpscreen")
 
 
 for (let index = 0; index < chiffre.length; index++) {
-    chiffre[index].addEventListener('click',()=>{
+    chiffre[index].addEventListener('click',()=>{                   //! boucle pour trigger tout les boutons et l afficher dans l input ! 
         inpscreen.value=inpscreen.value+chiffre[index].innerText
     })
 }
+
+
+
 btncalc[3].addEventListener('click', ()=>{
-    inpscreen.value=""
+    inpscreen.value=""                         //!bouton AC pour clear l input
 
 })
 
 let btnegal
 btncalc[18].addEventListener('click', ()=>{
-    btnegal=inpscreen.value
+    btnegal=inpscreen.value                        //! le bouton =
     console.log(btnegal.charAt(0));
     // if (btnegal.charAt(0)=".") {
     //     // console.log(btnegal);
     //     // console.log(btnegal.charAt(0));
-    //     inpscreen.value="veullez rentrer un chiffre en premier"
+    //     inpscreen.value="veullez rentrer un chiffre en premier"  //! j ai tester un peu la robustesse a paufiner
     // }else{
-
-    
-    
-    inpscreen.value=eval(btnegal)
-    
-    
+    inpscreen.value=eval(btnegal) 
 })
 
-// if (inpscreen.value=".") {
-//     inpscreen.value="veullez rentrer un chiffre en premier"
-// }
